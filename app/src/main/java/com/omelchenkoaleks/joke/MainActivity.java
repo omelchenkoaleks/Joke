62caf967f4bd9eff5c9bc1183280ef2153dc1a1f
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-16 08:13:37 +0300

    код, необходимый для запуска службы из активности

--- app/src/main/java/com/omelchenkoaleks/joke/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/joke/MainActivity.java
@@ -1,7 +1,9 @@
 package com.omelchenkoaleks.joke;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        // добавляем текст в интент
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE, getResources().getString(R.string.response));
+
+        // сервис запускается этой командой
+        startService(intent);
     }
 }
